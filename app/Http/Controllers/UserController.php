a88467964b4fb0c30c8367062e103944ef92739d
sahil43hb <sahilmubeen30@gmail.com>
2024-06-02 18:00:49 +0100

    first commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Mail\UserEmail;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Mail;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return response()->json(User::latest()->get());
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        // User::create([
+        //     'name' => $request->name,
+        //     'email' => $request->email,
+        //     'password' => $request->password
+        // ]);
+
+        $emailVar = [
+            'name' => $request->name,
+            'email' => $request->email,
+            'pass' => $request->password,
+        ];
+
+
+        $user = new User;
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = $request->password;
+        $res = $user->save();
+        if ($res) {
+            Mail::to($request->email)->send(new UserEmail($emailVar));
+            return response()->json('SuccessFully');
+        } else {
+            return response()->json('Not Added');
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        return response()->json(User::where('id', $id)->first());
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $user = User::where('id', $id)->first();
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->save();
+        return response()->json('SuccessFully');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $user = User::where('id', $id)->first();
+        $user->delete();
+        return response()->json('Delete Successfully');
+    }
+}
---
+++ resources/views/email/email.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h2>Register Email</h2>
+    <div>
+    <h3>Hi,{{$emailVar['name']}}</h3>
+    <h3>Your email is : {{$emailVar['email']}}</h3>
+    <h3>Your Password is:{{$emailVar['pass']}}</h3>
+    <h4>Congratulation!!! You are Register Our Account</h4>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cupiditate quo, vero sit minus adipisci pariatur quasi maiores molestias! Quam ratione est, voluptatum nostrum explicabo earum dolorem rem commodi exercitationem aliquid.</p>
+</div>
+</body>
+</html>
